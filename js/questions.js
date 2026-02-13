09454bfd51e9c8f052eb3e646b3a726e2b78045d
Mardepic <kivela.marika@gmail.com>
2026-02-13 23:02:06 +0200

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,1 +1,32 @@
+// js/questions.js
 
+export const QUESTIONS = [
+  // Viikko 1
+  "Mikä saa sinut tuntemaan itsesi eläväksi?",
+  "Millainen haluaisit olla kymmenen vuoden päästä?",
+  "Mikä on paras neuvo, jonka olet koskaan saanut?",
+  "Mistä asiasta olet viimeksi muuttanut mielipidettäsi?",
+  "Mikä lapsuusmuistosi on kirkkain?",
+  "Millaista rakkautta haluat elämääsi?",
+  "Mikä pelko estää sinua tällä hetkellä?",
+
+  // Viikko 2
+  "Kenelle olet kiitollinen ja miksi?",
+  "Mikä on suurin saavutuksesi tähän mennessä?",
+  "Millainen on täydellinen päiväsi?",
+  "Mitä haluaisit oppia tänä vuonna?",
+  "Mikä asia sinussa on muuttunut viimeisen vuoden aikana?",
+  "Milloin viimeksi teit jotain ensimmäistä kertaa?",
+  "Mikä on tärkein arvosi?",
+
+  // Viikko 3
+  "Keneltä haluaisit pyytää anteeksi?",
+  "Mikä unelma tuntuu mahdottomalta?",
+  "Millaisissa tilanteissa olet parhaimmillasi?",
+  "Mitä sinulle merkitsee menestys?",
+  "Mikä ominaisuutesi on vahvuutesi?",
+  "Mistä luovut helposti ja mistä et?",
+  "Millainen ympäristö saa sinut voimaan hyvin?"
+
+  // 🔹 Jatka samalla tavalla viikkoihin 52 asti
+];
